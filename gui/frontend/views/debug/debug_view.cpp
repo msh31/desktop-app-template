99e9f087c19c5865daf76822b38dc31058940069
Marco <marco@marco007.dev>
2026-08-23 00:57:41 +0200

    feat(gui->views->debug): add file picker example
    also stolen from savemanager lol

--- gui/frontend/views/debug/debug_view.cpp
+++ gui/frontend/views/debug/debug_view.cpp
@@ -1,4 +1,8 @@
 #include "debug_view.hpp"
+#include <utils/utils.hpp>
+#include <nfd.h>
+
+#include <frontend/childguard.hpp>
 
 #include <frontend/dialogs/confirm/confirm_dialog.hpp>
 #include <frontend/notification/notification.hpp>
@@ -7,41 +11,110 @@
 void CDebugView::render( ) {
     m_task_runner.update( ); // needs a refactor but fine for now | TODO: copy from SM
 
-    if ( ImGui::Button( "Click Me" ) ) {
-        Notify::show_notification( "", "Click Me button has been clicked!", 2000 );
+    {
+        ChildGuard buttons( "buttons", { 0.0f, 0.0f }, ImGuiChildFlags_Borders | ImGuiChildFlags_AutoResizeY );
+
+        if ( ImGui::Button( "Click Me" ) ) {
+            Notify::show_notification( "", "Click Me button has been clicked!", 2000 );
+        }
+
+        if ( ImGui::Button( "  \xef\x80\x81  Test Icon" ) ) {
+            Notify::show_notification( "", "Icon button has been clicked!", 2000 );
+        }
+
+        if ( ImGui::Button( "Test Notification" ) ) {
+            Notify::show_notification( "I am a title", "I am the body", 2000 );
+        }
+
+        if ( ImGui::Button( "Test confirm dialog" ) ) {
+            ConfirmDialog::show(
+                "Are you sure?", [this] { Notify::show_notification( "Test", "This is a test", 1000 ); } );
+        }
+
+        if ( ImGui::Button( "Test async task" ) ) {
+            m_task_runner.run(
+                [] { std::this_thread::sleep_for( std::chrono::seconds( 2 ) ); },
+                [] { Notify::show_notification( "Async", "Task complete!", 2000 ); },
+                []( const std::exception& ex ) { Notify::show_notification( "Error", ex.what( ), 5000 ); } );
+        }
     }
-    ImGui::Separator( );
-    if ( ImGui::Button( "  \xef\x80\x81  Test Icon" ) ) {
-        Notify::show_notification( "", "Icon button has been clicked!", 2000 );
-    }
+
     ImGui::SameLine( );
-    ImGui::Text( "\xef\x80\x88 \xef\x80\xad \xef\x83\xa9" );
 
-    if ( ImGui::Button( "Test Notification" ) ) {
-        Notify::show_notification( "I am a title", "I am the body", 2000 );
+    {
+        ChildGuard scaling( "scaling", { 0.0f, 0.0f }, ImGuiChildFlags_Borders | ImGuiChildFlags_AutoResizeY );
+        ImGui::Text( "Scaling" );
+
+        
+        if ( ImGui::Button( "Set scale to 2.0x" ) ) {
+            ThemeManager::apply_scale( 2.0f );
+        }
+        ImGui::SameLine( );
+        if ( ImGui::Button( "Set scale to 1.5x" ) ) {
+            ThemeManager::apply_scale( 1.5f );
+        }
+        ImGui::SameLine( );
+        if ( ImGui::Button( "reset scale" ) ) {
+            ThemeManager::apply_scale( 1.0f );
+        }
     }
 
-    if ( ImGui::Button( "Test confirm dialog" ) ) {
-        ConfirmDialog::show( "Are you sure?", [this] { Notify::show_notification( "Test", "This is a test", 1000 ); } );
-    }
+    {
+        ChildGuard filepicker( "filepicker", { 0.0f, 0.0f }, ImGuiChildFlags_Borders | ImGuiChildFlags_AutoResizeY );
+        ImGui::Text( "File Picker" );
 
-    if ( ImGui::Button( "Test async task" ) ) {
-        m_task_runner.run(
-            [] { std::this_thread::sleep_for( std::chrono::seconds( 2 ) ); },
-            [] { Notify::show_notification( "Async", "Task complete!", 2000 ); },
-            []( const std::exception& ex ) { Notify::show_notification( "Error", ex.what( ), 5000 ); } );
-    }
+        if ( ImGui::Button( "Open" ) ) {
+            NFD_Init( );
 
-    if ( ImGui::Button( "Set scale to 2.0x" ) ) {
-        ThemeManager::apply_scale( 2.0f );
+            nfdu8char_t* outPath;
+            //nfdu8filteritem_t filters[1] = { { "GTA SAN ANDREAS SAVE FILE", "b" } };
+            //nfdopendialogu8args_t args = { 0 };
+            //args.filterList = filters;
+            //args.filterCount = 1;
+            nfdresult_t result = NFD_OpenDialogU8( &outPath, nullptr, 0, nullptr ); //_With( &outPath, &args );
+            if ( result == NFD_OKAY ) {
+                std::string path( outPath );
+                NFD_FreePathU8( outPath );
+                m_file.open( path );
+                if ( m_file.is_open( ) ) {
+                    m_file_path = path;
+                    m_file_size = utils::format_file_size( fs::file_size( m_file_path ) );
+                    m_file_name = fs::path( path ).filename( ).string( );
+                    m_is_file_open = true;
+                    Notify::show_notification( "File Picker", "File loaded successfully!", 3000 );
+                } else {
+                    Notify::show_notification( "File Picker", "File failed to load!", 3000 );
+                }
+            } else if ( result == NFD_CANCEL ) {
+            } else {
+                Notify::show_notification( "File Picker", "File failed to load!", 3000 );
+            }
+
+            NFD_Quit( );
+        }
+        if ( m_is_file_open ) {
+            ImGui::SameLine( );
+            if ( ImGui::Button( "Close" ) ) {
+                m_file.close( );
+                m_file_path.clear( );
+                m_file_name.clear( );
+                m_file_size.clear( );
+                m_is_file_open = false;
+            }
+
+            ImGui::Separator( );
+
+            ImGui::TextDisabled( "Filename: %s", m_file_name.c_str( ) );
+            ImGui::TextDisabled( "Path: %s", m_file_path.c_str( ) );
+            ImGui::TextDisabled( "Size: %s",  m_file_size.c_str()); //path can never be a directory, guaranteed by the NFD dependency
+        }
     }
-    ImGui::SameLine( );
-    if ( ImGui::Button( "Set scale to 1.5x" ) ) {
-        ThemeManager::apply_scale( 1.5f );
-    }
-    ImGui::SameLine( );
-    if ( ImGui::Button( "reset scale" ) ) {
-        ThemeManager::apply_scale( 1.0f );
+}
+
+void CDebugView::on_exit() {
+    if ( m_file.is_open( ) ) {
+        m_file.close( );
+            m_file_path.clear( );
     }
 }
 
--- gui/frontend/views/debug/debug_view.hpp
+++ gui/frontend/views/debug/debug_view.hpp
@@ -6,7 +6,14 @@ class CDebugView : public CBaseView {
     public:
         ~CDebugView( ) override;
         void render( ) override;
+        void on_exit( ) override;
 
     private:
         CTaskRunner m_task_runner;
+
+        std::ifstream m_file;
+        std::string m_file_path;
+        std::string m_file_size; //shown with the proper uh suffix like GB, KB etc..
+        std::string m_file_name;
+        bool m_is_file_open = false;
 };
