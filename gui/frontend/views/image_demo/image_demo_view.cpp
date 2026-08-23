f59bf788f86f84386bdacbb709b3efed2dcd12c2
Marco <marco@marco007.dev>
2026-08-23 19:17:31 +0200

    feat(gui->image_demo): add demo of both image draw methods
    part 2 of the previous commit

--- gui/frontend/views/image_demo/image_demo_view.cpp
+++ gui/frontend/views/image_demo/image_demo_view.cpp
@@ -3,22 +3,49 @@
 
 #include <frontend/childguard.hpp>
 #include <frontend/images/pepe.hpp>
+#include <frontend/ui.hpp>
 
-void CImageDemoView::on_enter( ) { 
+void CImageDemoView::on_enter( ) {
 	m_embedded = CImageManager::get( ).load_from_memory( pepe_png, pepe_png_len, "pepe" );
 };
 
-void CImageDemoView::render( ) { 
+void CImageDemoView::render( ) {
     ImGui::Text( "Hello, this is the image demo view" );
 
     auto avail = ImGui::GetContentRegionAvail( );
-    auto scale = std::min( avail.x / m_embedded.texture_width, avail.y / m_embedded.texture_height );
+    ImVec2 child_size( ( avail.x - ImGui::GetStyle( ).ItemSpacing.x ) * 0.5f, avail.y );
 
-	{
-        ChildGuard pepe( "pepe", { 0.0f, 0.0f } );
+    {
+        ChildGuard cover( "pepe_cover", child_size, ImGuiChildFlags_Borders );
+        ImGui::TextDisabled( "cover" );
 
-        ImGui::Image((ImTextureID)m_embedded.texture_id, ImVec2( m_embedded.texture_width * scale, m_embedded.texture_height * scale) );
-	}
+        auto p_min = ImGui::GetCursorScreenPos( );
+        auto size = ImGui::GetContentRegionAvail( );
+
+        ui::add_cover_image(
+            ImGui::GetWindowDrawList( ), (ImTextureID)m_embedded.texture_id, p_min,
+            ImVec2( p_min.x + size.x, p_min.y + size.y ), (float)m_embedded.texture_width,
+            (float)m_embedded.texture_height );
+
+        ImGui::Dummy( size );
+    }
+
+    ImGui::SameLine( );
+
+    {
+        ChildGuard contain( "pepe_contain", child_size, ImGuiChildFlags_Borders );
+        ImGui::TextDisabled( "contain" );
+
+        auto p_min = ImGui::GetCursorScreenPos( );
+        auto size = ImGui::GetContentRegionAvail( );
+
+        ui::add_contain_image(
+            ImGui::GetWindowDrawList( ), (ImTextureID)m_embedded.texture_id, p_min,
+            ImVec2( p_min.x + size.x, p_min.y + size.y ), (float)m_embedded.texture_width,
+            (float)m_embedded.texture_height );
+
+        ImGui::Dummy( size );
+    }
 }
 
 void CImageDemoView::on_exit( ) {
