b6801e4341e39f63c0225793c627e68194294e3e
Marco <marco@marco007.dev>
2026-08-27 16:49:35 +0200

    chore:(gui->frontend): remove add_colored_text and swap string_view for const string refs

--- gui/frontend/ui.hpp
+++ gui/frontend/ui.hpp
@@ -2,39 +2,30 @@
 
 namespace ui {
     // text
-    inline void add_colored_text( std::string_view text, ImVec4 color ) {
-        ImGui::PushStyleColor( ImGuiCol_Text, color );
-        ImGui::Text( "%s", text.data( ) );
-        ImGui::PopStyleColor( );
-
-        // hmmm, this exists too.
-        //  ImGui::TextColored(ImColor(198, 97, 63).Value, "%s", std::string(text).c_str());
-    }
-
-    inline void add_font_text( std::string_view text, ImFont* font ) {
+    inline void add_font_text( const std::string& text, ImFont* font ) {
         ImGui::PushFont( font );
-        ImGui::Text( "%s", std::string( text ).c_str( ) );
+        ImGui::Text( "%s", text.c_str() );
         ImGui::PopFont( );
     }
 
-    inline void add_styled_text( std::string_view text, ImGuiStyleVar_ style, float style_size ) {
+    inline void add_styled_text( const std::string& text, ImGuiStyleVar_ style, float style_size ) {
         ImGui::PushStyleVar( style, style_size );
-        ImGui::Text( "%s", std::string( text ).c_str( ) );
+        ImGui::Text( "%s", text.c_str( ) );
         ImGui::PopStyleVar( );
     }
 
     // buttons
-    inline void add_danger_button( std::string_view text, ImVec2 size, std::function<void( )> fun ) {
+    inline void add_danger_button( const std::string& text, ImVec2 size, std::function<void( )> fun ) {
         ImGui::PushStyleColor( ImGuiCol_Button, ImVec4( 0.8f, 0.2f, 0.2f, 1.0f ) );
         ImGui::PushStyleColor( ImGuiCol_ButtonHovered, ImVec4( 0.9f, 0.3f, 0.3f, 1.0f ) );
 
-        if ( ImGui::Button( text.data( ), size ) ) fun( );
+        if ( ImGui::Button( text.c_str( ), size ) ) fun( );
         ImGui::PopStyleColor( 2 );
     }
 
-    inline void add_small_button( std::string_view text, std::function<void( )> fun ) {
+    inline void add_small_button( const std::string& text, std::function<void( )> fun ) {
         ImGui::PushStyleVar( ImGuiStyleVar_FramePadding, ImVec2( 3.0f, 3.0f ) );
-        if ( ImGui::Button( text.data( ) ) ) fun( );
+        if ( ImGui::Button( text.c_str( ) ) ) fun( );
         ImGui::PopStyleVar( );
     }
 
