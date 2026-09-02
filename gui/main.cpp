b87cd1a1b3f375edf81c6ad7211a244a9a97198e
Marco <marco@marco007.dev>
2026-09-02 22:22:22 +0200

    fix(gui->entrypoint): setup logger after setting config dir
    lmao - from sm.

--- gui/main.cpp
+++ gui/main.cpp
@@ -10,8 +10,8 @@
 int main( ) {
     curl_global_init( CURL_GLOBAL_ALL );
     try {
-        setup_logger( );
         paths::set_config_dir( paths::exe_dir( ) / "config" );
+        setup_logger( );
 
         SPDLOG_INFO( "==============={}===============", APP_NAME );
         CConfig config;
