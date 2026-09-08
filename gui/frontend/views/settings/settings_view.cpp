5e5c234cc7f53062ab2ebf1559a2262ddfc9431e
Marco <marco@marco007.dev>
2026-09-08 21:02:07 +0200

    fix(gui->settings): guard BG folder iteration with an is_directory check in on_enter()
    this is such a fucking nitpick

--- gui/frontend/views/settings/settings_view.cpp
+++ gui/frontend/views/settings/settings_view.cpp
@@ -10,8 +10,10 @@
 void CSettingsView::on_enter( ) {
     m_backgrounds.clear( );
 
-    // unlikely to happen but guard it anyway
-    if ( fs::exists( paths::backgrounds_dir( ) ) ) {
+    //this is over engineered as hell.
+    //by this point, the program will have created this DIRECTORY itself and therefore IS GUARANTEED to be a directory.
+    // i hate this.
+    if ( fs::exists( paths::backgrounds_dir( ) ) && fs::is_directory( paths::backgrounds_dir() ) ) {
         for ( const auto& f :
               fs::directory_iterator( paths::backgrounds_dir( ), fs::directory_options::skip_permission_denied ) ) {
             if ( f.is_regular_file( ) && utils::is_image_file( f.path( ) ) ) {
